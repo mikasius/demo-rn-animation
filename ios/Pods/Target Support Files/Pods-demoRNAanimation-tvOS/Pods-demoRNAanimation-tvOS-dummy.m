7e96ff8044513bbc44e7cf773656fb103af138ef
Sergey Metlenkin <sergei.metlenkin@mcc-tomsk.de>
2020-05-16 21:50:24 +0700

    Initial commit

---
+++ ios/Pods/Target Support Files/Pods-demoRNAanimation-tvOS/Pods-demoRNAanimation-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_demoRNAanimation_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_demoRNAanimation_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-demoRNAanimation-tvOSTests/Pods-demoRNAanimation-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_demoRNAanimation_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_demoRNAanimation_tvOSTests
+@end
